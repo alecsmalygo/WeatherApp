1cf3eda1042e1efa469e7727f5a7fae5b4e28d2a
Alec Smalygo <ninjaalec12@gmail.com>
2021-04-26 09:26:00 -0500

    Initial Commit

---
+++ WeatherAppAppAppApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherAppAppAppApp
+//
+//  Created by This Is Alec's Account on 4/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
